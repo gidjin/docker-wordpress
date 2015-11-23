71364c4d158cd95d27d7b65b53d339e6c722c8c1
John Gedeon <js1@gedeons.com>
2015-11-22 21:22:47 -0800

    Correct forgotten path

--- bin/msmtprc_generator.rb
+++ bin/msmtprc_generator.rb
@@ -36,6 +36,9 @@ puts "Rendering msmtprc"
 renderer = ERB.new(File.read("/templates/msmtprc.erb"))
 File.write("/etc/php5/apache2/msmtprc", renderer.result())
 File.write("/etc/php5/cli/msmtprc", renderer.result())
+
 FileUtils.chmod("u=rw,go-rw", "/etc/php5/apache2/msmtprc", :verbose => true)
 FileUtils.chmod("u=rw,go-rw", "/etc/php5/cli/msmtprc", :verbose => true)
-FileUtils.chown("www-data", "www-data", "/usr/local/etc/msmtprc", :verbose => true)
+
+FileUtils.chown("www-data", "www-data", "/etc/php5/apache2/msmtprc", :verbose => true)
+FileUtils.chown("www-data", "www-data", "/etc/php5/cli/msmtprc", :verbose => true)
