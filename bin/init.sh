b7e1b47e1cf3118bd27b408ded1f7daa51035a69
John Gedeon <js1@gedeons.com>
2015-12-05 11:17:49 -0800

    Clean up comments

--- bin/init.sh
+++ bin/init.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -l
-set -e
 
-# if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
-  # /usr/local/bin/etcd_updater_service.rb start
-  /usr/sbin/cron
-  /usr/local/rvm/gems/ruby-2.2.3/bin/whenever -u www-data -w -f /schedule.rb
-  /usr/local/bin/msmtprc_generator.rb
-# fi
+# un-official strict mode
+set -euo pipefail
 
-# exec /entrypoint.sh "$@"
+/usr/sbin/cron
+/usr/local/rvm/gems/ruby-2.2.3/bin/whenever -u www-data -w -f /schedule.rb
+/usr/local/bin/msmtprc_generator.rb
+
 exec /run.sh
